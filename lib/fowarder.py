65e52b87262a9a6e35da4123cbff1155d3758a5f
drops <dropsql@protonmail.com>
2021-04-18 18:17:32 +0200

    all versions support

--- lib/fowarder.py
+++ lib/fowarder.py
@@ -19,25 +19,22 @@ def foward(source, destination, direction):
                 buf += _data
 
                 if direction: # client to server packet
+                    
                     packet_id, data = varint_unpack(_data)
                     packet_id = hex(packet_id)
 
 
                     packet_id, data = varint_unpack(_data[1:] if _data.startswith(varint_pack(0)) else _data)
                     packet_id = hex(packet_id)
-                    
-                    if packet_id == '0x1':
-                        try:
-                            lenght, message = varint_unpack(data)
-                            message = message.decode('utf-8')
-                            for regex in regexs:
-                                if re.match(regex, message):
-                                    print_success('got a match !!! \'%s\'' % message)
-                                    break
-                            else:
-                                print_info('catched a message from client: \'%s\'' % message)
-                        except:
-                            pass
+                    try:
+                        lenght, message = varint_unpack(data)
+                        message = message.decode('utf-8')
+                        for regex in regexs:
+                            if re.match(regex, message):
+                                print_success('got a match !!! \'%s\'' % message)
+                                break
+                    except:
+                        pass
 
                 destination.send(buf)
         except Exception as e:
